76a613a3a90b3c0a9fceda35ee603cafb065144a
silkemarckx <silke-marckx@hotmail.com>
2016-12-22 16:38:16 +0100

    Tokenizer+POS
    Comparing 2 POS taggers on a text file

--- PD.py
+++ PD.py
@@ -1,5 +1,20 @@
+import nltk
+from nltk import word_tokenize, pos_tag
+import re
+from nltk.corpus import brown, ptb #http://www.nltk.org/howto/corpus.html
+
+
 #Brief overview
 #1. tokenization
+f = open('obama.txt','rt', encoding='utf-8') #http://www.americanrhetoric.com/speeches/barackobama/barackobamawhitehousecorrespondentsdinner2016.htm
+text=f.read()
+tokenized= re.split(r'\W+', text)
+tokenized= word_tokenize(text)
+print(pos_tag(tokenized))
+
+brown_tagged_sents = brown.tagged_sents(categories='news')
+unigram_tagger = nltk.UnigramTagger(brown_tagged_sents)
+print(unigram_tagger.tag(tokenized))
 #2. Total word count
 #3. Part of speech tagger
 #4. Rules
