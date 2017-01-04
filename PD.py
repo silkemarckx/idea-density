bd83cd3ed0825b27d914252b77cb6401d07bf29d
silkemarckx <silke-marckx@hotmail.com>
2017-01-04 20:31:53 +0100

    Range & visualisation output
    Still problem with "inderx out of range" when used with different
    texts!!

--- PD.py
+++ PD.py
@@ -1,28 +1,25 @@
 import nltk
 from nltk import word_tokenize, pos_tag
 import re
-from nltk.corpus import brown, ptb, treebank #http://www.nltk.org/howto/corpus.html
 
-def IdeaDensity(inputtext):
+def IdeaDensity(inputtext, rnge=0):
 	#1. tokenization & part-of-speech tagging --> normally Penn Treebank
 	f = open(inputtext,'rt', encoding='utf-8') #http://www.americanrhetoric.com/speeches/barackobama/barackobamawhitehousecorrespondentsdinner2016.htm
 	text=f.read()
 	f.close()
-	#tokenized= re.split(r'\W+', text)
 
 	tokenized= word_tokenize(text)
 	taggedtext = pos_tag(tokenized)
-	print('Tagged text: Original',taggedtext)
+	Originaltext= taggedtext
+	#print('Tagged text: Original',taggedtext)
 
 
 	#2. Total word count --> Multiplication: no words!
-
 	#Deleting punctuation
 	#Problem: Only removes all punctuation after a second identical loop. 
 	#=> OVERALL PROBLEM: 2 items with same tag: No removal of second item!!
-	Punctuation = (',','.','!',';','?',':',"'")
+	Punctuation = (',','.','!',';','?',':',"'","''",'``')
 	for word in taggedtext:
-		#regexp= re.compile(r'\,|\.|\;') PROBLEM!
 		if word[0] in Punctuation:
 			taggedtext.remove(word)
 		else:
@@ -34,8 +31,12 @@ def IdeaDensity(inputtext):
 			continue
 
 	wordcount = len(taggedtext)
-	print('wordcount: ',wordcount)
+	#print('wordcount: ',wordcount)
 
+	if wordcount > 2000:
+		rnge +=40
+	elif wordcount >1000:
+		rnge+=4
 
 	#3. Proposition count & Adjustment Rules
 	"""= verbs
@@ -54,8 +55,8 @@ def IdeaDensity(inputtext):
 	'a,an,the' =/ prop
 	models =/ prop, UNLESS negative (ending in "n't")
 	COPULA + NP ==> copula = prop 
-	COPULA + AdjP ==> copula(linking verb) =/ prop  (AdjP=prop!)
-	Remove subject-auxiliary inversion"""
+	COPULA + AdjP ==> copula(linking verb) =/ prop  (AdjP=prop!)"""
+	propcount= len(taggedtext)
 
 	#Removing positive modals from prop count.
 	#For negative modals: Modal is removed, negative part carries prop count & is not removed(otherwise: double count)
@@ -70,8 +71,7 @@ def IdeaDensity(inputtext):
 			taggedtext.remove(word)
 		else:
 			continue
-	propcount=(len(taggedtext))
-	for i in range(1,(propcount)-1):
+	for i in range(1,(len(taggedtext)-(1+rnge))):
 		if taggedtext[i] == ('to', 'TO'): 
 			if taggedtext[i-1] == ('got', 'VBN'):
 				#print(taggedtext[i-1],taggedtext[i])
@@ -83,7 +83,7 @@ def IdeaDensity(inputtext):
 
 	ModalNeed= (('need', 'VB'),('Need', 'VB'),('need', 'VBP'),('Need', 'VBP'), ('need', 'MD'),('needed', 'VBD'),('Needed', 'VBD'))
 	Negative= (("n't", 'RB'),('not', 'RB'))
-	for i in range(1,(len(taggedtext))-5): 
+	for i in range(1,(len(taggedtext)-(5+rnge))): 
 		if taggedtext[i-1] in ModalNeed: 
 			if taggedtext[i] != ('to', 'TO'): #Following the original grammar rules: need + to = main verb, instead of semi-modal
 				if taggedtext[i] in Negative:
@@ -107,12 +107,12 @@ def IdeaDensity(inputtext):
 				continue
 		else:
 			continue
-	for i in range(1,(len(taggedtext))-6): 
+	for i in range(1,(len(taggedtext)-(6+rnge))): 
 		if taggedtext[i-1] in ModalNeed: 
 			if taggedtext[i] != ('to', 'TO'): 
 				for word in taggedtext[i+1]: #problem: '... need her. Do' --> need=removed!
 					if word == 'VB':
-						print('LAST',taggedtext[i+1],len(taggedtext))
+						#print('LAST',taggedtext[i+1],len(taggedtext))
 						taggedtext.remove(taggedtext[i-1])
 					else:
 						continue
@@ -121,59 +121,17 @@ def IdeaDensity(inputtext):
 		else:
 			continue
 
-	#removing aux 'used/able/going/need to', not a MODAL! (but must be removed before 'to' is removed)
-	Xto = (('used', 'VBD'),('going', 'VBG'),('able', 'JJ'),('need', 'VB'),('need', 'VBP'),('needs', 'VBZ'))
-	for i in range(1,(len(taggedtext))-7): 
-		if taggedtext[i-1] in Xto: 
-			if taggedtext[i] == ('to', 'TO'):
-				for word in taggedtext[i+1]:
-					if word == 'VB':
-						word ="I need to rename this, in order to avoid removing all TO's"
-						taggedtext.remove(taggedtext[i-1])
-			else:
-				continue			
-		else:
-			continue
-	
-
-	print('Propositions -Modals: ',taggedtext)
+	#print('Propositions -Modals: ',taggedtext)
+	modalcount = propcount-len(taggedtext)
+	#print('#removed modals: ', modalcount)
 	propcount= len(taggedtext)
-	print('Proposition Count -modals: ', propcount)
 
-	#Removing 'To' in 'to+verb'
-	#Problem: Only removes all 'To'(+verb) after second identical loop. (=Overall problem)
-	for i in range(1,(len(taggedtext))-12):
-		if taggedtext[i-1] == ('to', 'TO'): 
-			for word in taggedtext[i]:
-				if word == 'VB':
-					#print(taggedtext[i])
-					taggedtext[i-1]="I need to rename this, in order to avoid removing al TO's"
-					taggedtext.remove(taggedtext[i-1])
-				else:
-					continue
-		else:
-			continue
-	for i in range(1,(len(taggedtext))-3): #removing 'to' bij 'to+verb' 
-		if taggedtext[i-1] == ('to', 'TO'): 
-			for word in taggedtext[i]:
-				if word == 'VB':
-					#print(taggedtext[i-1],taggedtext[i])
-					taggedtext.remove(taggedtext[i-1])
-				else:
-					continue
-		else:
-			#print(taggedtext[i],len(taggedtext))
-			continue
-
-	print('Propositions (-to): ',taggedtext)
-	propcount= len(taggedtext)
-	print('Proposition Count -to: ', propcount)
 
 	#Removing auxiliary verbs
 	#Problem: sentences like 'Why did you tell her that...' --> 'did' is NOT removed
 	AuxDO= (('do', 'VBP'),('Do', 'VBP'),('does', 'VBZ'),('Does', 'VBZ'),('did', 'VBD'),('Did', 'VBD'))
 	FollowingAux = (("n't", 'RB'),('not', 'RB'),('been', 'VBN'))
-	for i in range(1,(len(taggedtext))-2): 
+	for i in range(5,(len(taggedtext)-(2+(100*rnge)))): 
 		if taggedtext[i] in FollowingAux: 
 			if taggedtext[i-1] in AuxDO:
 				#print(taggedtext[i-1],taggedtext[i])
@@ -184,14 +142,15 @@ def IdeaDensity(inputtext):
 		if (word == ('Do','VBP') or word == ('Does', 'VBZ')):
 			#print(word)
 			taggedtext.remove(word)
+	DOcount = propcount-len(taggedtext)
+	#print('#removed Do-verbs: ', DOcount)
 	propcount= len(taggedtext)
-	print('Proposition Count -DO: ', propcount)
 
 
 	AuxHAVE= (('have', 'VBP'),('Have', 'VBP'),('has', 'VBZ'),('Has', 'VBZ'),("'ve", 'VBP'),
 		('had', 'VBD'),('Had', 'VBD'),('have', 'VB'))
-	FolHAVE=('VB','VBN')
-	for i in range(1,(len(taggedtext))-6): 
+	FolHAVE=('VBN','TO')
+	for i in range(1,(len(taggedtext)-(6+10*rnge))): 
 		if taggedtext[i] in FollowingAux: 
 			if taggedtext[i-1] in AuxHAVE:
 				#print(taggedtext[i-1],taggedtext[i])
@@ -211,15 +170,16 @@ def IdeaDensity(inputtext):
 					continue
 		else:
 			continue
+	HAVEcount = propcount-len(taggedtext)
+	#print('#removed HAVE-verbs: ', HAVEcount)
 	propcount= len(taggedtext)
-	print('Proposition Count -HAVE: ', propcount)
 
 
 	AuxBE= (('am', 'VBP'),('Am', 'VBP'),("'m", 'VBP'),('are', 'VBP'),('Are', 'VBP'),("'re", 'VBP'),
 		('is','VBZ'),('Is','VBZ'),("'s",'VBZ'),('was','VBD'),('Was','VBD'),('were','VBD'),
 		('Were','VBD'),('been','VBN'),('Been','VBN'),('being','VBG'),('Being','VBG'),('be', 'VB'))
-	FolBE= ('VBG','VBN','VB') #VB --> Modal 'be to +verb', 'to' is already removed, so: be+ verb (& verb = VB)
-	for i in range(1,(len(taggedtext))-9): 
+	FolBE= ('VBG','VBN','TO') #TO --> Modal 'be to +verb'
+	for i in range(1,(len(taggedtext)-(9+rnge))): 
 		if taggedtext[i-1] in AuxBE:
 			for word in taggedtext[i]:
 				if word in FolBE:
@@ -242,41 +202,90 @@ def IdeaDensity(inputtext):
 					continue
 		else:
 			continue
+	BEcount = propcount-len(taggedtext)
+	#print('#removed BE-verbs: ', BEcount)
+	propcount= len(taggedtext)
 
 	AuxGET = (('get', 'VBP'), ('gets', 'VBZ'),('got', 'VBD'))
-	for i in range(1,(len(taggedtext))-5): 
+	for i in range(1,(len(taggedtext)-(5+rnge))): 
 		if taggedtext[i-1] in AuxGET:
 			for word in taggedtext[i]:
 				if word == 'VBN':
 					#print('FIRST',taggedtext[i-1],taggedtext[i])
 					taggedtext.remove(taggedtext[i-1])
 
-	print('Propositions -aux: ',taggedtext)
+	#removing aux 'used/able/going/need to'
+	Xto = (('used', 'VBD'),('going', 'VBG'),('able', 'JJ'),('need', 'VB'),('need', 'VBP'),('needs', 'VBZ'))
+	for i in range(1,(len(taggedtext)-(7+rnge))): 
+		if taggedtext[i-1] in Xto: 
+			if taggedtext[i] == ('to', 'TO'):
+				for word in taggedtext[i+1]:
+					if word == 'VB':
+						word ="I need to rename this, in order to avoid removing all TO's"
+						taggedtext.remove(taggedtext[i-1])
+			else:
+				continue			
+		else:
+			continue
+	#print('Propositions -aux: ',taggedtext)
+	AUXcount = propcount-len(taggedtext)
+	#print('#removed other AUX-verbs: ', AUXcount)
 	propcount= len(taggedtext)
-	print('Proposition Count -AUX: ', propcount)
 
+	
+	#Removing 'To' in 'to+verb'
+	#Problem: Only removes all 'To'(+verb) after second identical loop. (=Overall problem)
+	FolTO= ('VB','VBG','VBN') #When aux have&be are removed-->> (VBG: have to be ..ing--> to ...ing) (VBN: ought to have ..ed --> to ..ed)
+	for i in range(1,(len(taggedtext)-(12+(2*rnge)))):
+		if taggedtext[i-1] == ('to', 'TO'): 
+			for word in taggedtext[i]:
+				if word in FolTO:
+					#print(taggedtext[i])
+					taggedtext[i-1]="I need to rename this, in order to avoid removing al TO's"
+					taggedtext.remove(taggedtext[i-1])
+				else:
+					continue
+		else:
+			continue
+	for i in range(1,(len(taggedtext)-(3+rnge))): #removing 'to' bij 'to+verb' 
+		if taggedtext[i-1] == ('to', 'TO'): 
+			for word in taggedtext[i]:
+				if word in FolTO:
+					#print(taggedtext[i-1],taggedtext[i])
+					taggedtext.remove(taggedtext[i-1])
+				else:
+					continue
+		else:
+			#print(taggedtext[i],len(taggedtext))
+			continue
+
+	TOcount = propcount-len(taggedtext)
+	#print("#removed TO's: ", TOcount)
+	propcount= len(taggedtext)
+
+	
 	#Removing prime copula 'to be', followed by AdjP
 	PrimeCopula = (('be','VB'),('am','VBP'),("'m",'VBP'),('are','VBP'),("'re",'VBP'),('is','VBZ'),("'s",'VBZ'),
 		('was','VBD'),('were','VBD'),('been','VBN'),('being','VBG'))
 	AdjP= ('JJ','JJR','JJS')
-	for i in range(1,(len(taggedtext))-1): 
+	for i in range(1,(len(taggedtext)-(1+rnge))): 
 		if taggedtext[i-1] in PrimeCopula:
 			for word in taggedtext[i]:
 				if word in AdjP:
-					print('Copula BE+ JJ.',taggedtext[i-1],taggedtext[i])
+					#print('Copula BE+ JJ.',taggedtext[i-1],taggedtext[i])
 					taggedtext[i-1]="to be removed"
 					taggedtext.remove(taggedtext[i-1])
 				else:
 					continue
 		else:
 			continue
-	for i in range(1,(len(taggedtext))-1): 
+	for i in range(1,(len(taggedtext)-(1+rnge))): 
 		if taggedtext[i-1] in PrimeCopula:
 			for word in taggedtext[i]:
 				if word == 'RB':
 					for word in taggedtext[i+1]:
 						if word in AdjP:
-							print('Copula BE+ RB+JJ.',taggedtext[i-1],taggedtext[i],taggedtext[i+1])
+							#print('Copula BE+ RB+JJ.',taggedtext[i-1],taggedtext[i],taggedtext[i+1])
 							taggedtext[i-1]="To be removed"
 							taggedtext.remove(taggedtext[i-1])
 						else:
@@ -285,7 +294,9 @@ def IdeaDensity(inputtext):
 					continue
 		else:
 			continue		
-
+	BECOPcount = propcount-len(taggedtext)
+	#print('#removed COPULA-verbs: ', BECOPcount)
+	propcount= len(taggedtext)
 
 	#Removing secondary copula, followed by AdjP
 	CopulaInchoative= (('become','VB'),('become','VBP'),('becomes','VBZ'),('became','VBD'),('become','VBN'),('becoming','VBG'),
@@ -308,61 +319,94 @@ def IdeaDensity(inputtext):
 		('seem','VB'),('seem','VBP'),('seems','VBZ'),('seemed','VBD'),('seemed','VBN'),('seeming','VBG'),
 		('sound','VB'),('sound','VBP'),('sounds','VBZ'),('sounded','VBD'),('sounded','VBN'),('sounding','VBG'),
 		('look','VB'),('look','VBP'),('looks','VBZ'),('looked','VBD'),('looked','VBN'),('looking','VBG'))
-	for i in range(1,(len(taggedtext))-6): 
+	for i in range(1,(len(taggedtext)-(6+rnge))): 
 		if (taggedtext[i-1] in CopulaInchoative or taggedtext[i-1] in CopulaSenses or taggedtext[i-1] in CopulaOthers):
 			for word in taggedtext[i]:
 				if word in AdjP:
-					print('Copula JJ.',taggedtext[i-1],taggedtext[i])
+					#print('Copula JJ.',taggedtext[i-1],taggedtext[i])
 					taggedtext.remove(taggedtext[i-1])
-	for i in range(1,(len(taggedtext))-2): 
+	for i in range(1,(len(taggedtext)-(2+rnge))): 
 		if (taggedtext[i-1] in CopulaInchoative or taggedtext[i-1] in CopulaSenses or taggedtext[i-1] in CopulaOthers):
 			for word in taggedtext[i]:
 				if word == 'RB':
 					for word in taggedtext[i+1]:
 						if word in AdjP:
-							print('Copula RB+JJ.',taggedtext[i-1],taggedtext[i],taggedtext[i+1])
+							#print('Copula RB+JJ.',taggedtext[i-1],taggedtext[i],taggedtext[i+1])
 							taggedtext.remove(taggedtext[i-1])
 				else:
 					continue
 		else:
 			continue
 
-	print('Propositions -COP: ',taggedtext)
+	#print('Propositions -COP: ',taggedtext)
+	COPcount = propcount-len(taggedtext)
+	#print('#removed COPULA-verbs: ', COPcount)
 	propcount= len(taggedtext)
-	print('Proposition Count -COP: ', propcount)
 
 	#Removing non-propositional tags + some determiners
 	#Problem: see overall problem. 3 identical loops needed!!
 	Removetags = ('NNP','NN','NNS','NNPS','PRP','RP','EX')
-	RemoveDT= ('a','A','an','An','the','The')
 	for word in taggedtext: #word= ('word','tag')
 		if word[1] in Removetags:
 			taggedtext.remove(word)
-		elif word[0] in RemoveDT:
+		else:
+			continue
+	for word in taggedtext: 
+		if word[1] in Removetags:
 			taggedtext.remove(word)
 		else:
 			continue
 	for word in taggedtext: 
 		if word[1] in Removetags:
 			taggedtext.remove(word)
-		elif word[0] in RemoveDT:
+		else:
+			continue
+	TAGcount = propcount-len(taggedtext)
+	#print('#removed non-prop tags: ', TAGcount)
+	propcount= len(taggedtext)
+
+	RemoveDT= ('a','A','an','An','the','The')
+	for word in taggedtext: #word= ('word','tag')
+		if word[0] in RemoveDT:
 			taggedtext.remove(word)
 		else:
 			continue
 	for word in taggedtext: 
-		if word[1] in Removetags:
+		if word[0] in RemoveDT:
 			taggedtext.remove(word)
-		elif word[0] in RemoveDT:
+		else:
+			continue
+	for word in taggedtext: 
+		if word[0] in RemoveDT:
 			taggedtext.remove(word)
 		else:
 			continue
 
-	print('Propositions -tags etc. : ',taggedtext)
+	#print('Propositions -tags etc. : ',taggedtext)
+	DTcount = propcount-len(taggedtext)
+	#print('#removed DT(a,an,the): ', DTcount)
 	propcount= len(taggedtext)
-	print('Proposition Count: ', propcount)
+	#print('Propositions : ',taggedtext)
 
 	#5. Measuring PD (#prop./#words)
 	propdensity= propcount/wordcount
-	print('Propositional Idea Density: ',propdensity)
+	print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>STATISTICS<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<',
+		'\n\nPropositional Idea Density: 	\t',propdensity,
+		'\n----------------------------------------------------------------------',
+		'\nOriginal tagged text: 	\t\t\t', Originaltext,
+		'\nTotal wordcount: 		\t\t\t', wordcount,'items',
+		'\nNumber of removed items (Absolute number, Relative number):',
+		'\n\t>> Modals: 			\t\t\t', modalcount,'\t\t',round(modalcount/wordcount,5),
+		'\n\t>> Auxiliary "to do": 	\t\t', DOcount,'\t\t',round(DOcount/wordcount,5),
+		'\n\t>> Auxiliary "to have": \t\t', HAVEcount,'\t\t',round(HAVEcount/wordcount,5),
+		'\n\t>> Auxiliary "to be": 	\t\t', BEcount,'\t\t',round(BEcount/wordcount,5),
+		'\n\t>> Remaining auxiliaries: 	\t', AUXcount,'\t\t',round(AUXcount/wordcount,5),
+		'\n\t>> "TO" followed by a verb: \t', TOcount,'\t\t',round(TOcount/wordcount,5),
+		'\n\t>> Copula "to be": 		\t\t', BECOPcount,'\t\t',round(BECOPcount/wordcount,5),
+		'\n\t>> Other copulas: 			\t', COPcount,'\t\t',round(COPcount/wordcount,5),
+		'\n\t>> Non-propositional tags:	\t', TAGcount,'\t',round(TAGcount/wordcount,5),
+		'\n\t>> "a,an,the"-determiners: \t\t', DTcount,'\t',round(DTcount/wordcount,5),
+		'\nTagged propositions: 		\t\t', taggedtext)
 
-IdeaDensity('testtext.txt')
+
+IdeaDensity('secondtext.txt')
--- test.py
+++ test.py
@@ -1,9 +1,64 @@
-a = 'not'
-b= "n't"
+import re
+from collections import Counter
+from nltk import word_tokenize, pos_tag
+from nltk.corpus import brown
 
-word= 'not'
 
-if not (word is a or word is  b):
-	print('not')
-else:
-	print('ok')
+def words(text): return re.findall(r'\w+', text.lower())
+
+WORDS = Counter(words(open('austen-emma.txt').read()))
+
+def P(word, N=sum(WORDS.values())): 
+    "Probability of `word`."
+    return WORDS[word] / N
+
+def correction(word): 
+    "Most probable spelling correction for word."
+    return max(candidates(word), key=P)
+
+def candidates(word): 
+    "Generate possible spelling corrections for word."
+    return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
+
+def known(words): 
+    "The subset of `words` that appear in the dictionary of WORDS."
+    return set(w for w in words if w in WORDS)
+
+def edits1(word):
+    "All edits that are one edit away from `word`."
+    letters    = 'abcdefghijklmnopqrstuvwxyz'
+    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+    deletes    = [L + R[1:]               for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+    inserts    = [L + c + R               for L, R in splits for c in letters]
+    return set(deletes + transposes + replaces + inserts)
+
+def edits2(word): 
+    "All edits that are two edits away from `word`."
+    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+
+f = open('testtext.txt','rt', encoding='utf-8') #http://www.americanrhetoric.com/speeches/barackobama/barackobamawhitehousecorrespondentsdinner2016.htm
+text=f.read()
+f.close()
+tokenized= word_tokenize(text)
+taggedtext = pos_tag(tokenized)
+
+Punctuation = (',','.','!',';','?',':',"'")
+for word in taggedtext:
+	#regexp= re.compile(r'\,|\.|\;') PROBLEM!
+	if word[0] in Punctuation:
+		taggedtext.remove(word)
+	else:
+		continue
+for word in taggedtext:
+	if word[0] in Punctuation:
+		taggedtext.remove(word)
+	else:
+		continue
+
+"""for word in taggedtext:
+	item=list(word)
+	if word not in brown.tagged_words():
+		#(word,'aw')
+		#print(word,correction(word))"""
